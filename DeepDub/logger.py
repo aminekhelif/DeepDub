5bafeb6fda311441b3a0d68f6f53ae9cc62a938b
Amine khelif <khelifaminekhelif@gmail.com>
2024-11-30 19:21:29 +0100

    ADD logger

---
+++ DeepDub/logger.py
@@ -0,0 +1,18 @@
+# logger.py
+import yaml
+import logging
+
+def setup_logger(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    logging_level = config.get('logging_level', 'INFO').upper()
+    numeric_level = getattr(logging, logging_level, None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {logging_level}')
+    
+    logging.basicConfig(level=numeric_level)
+    logger = logging.getLogger(__name__)
+    return logger
+
+logger = setup_logger('config.yaml')
